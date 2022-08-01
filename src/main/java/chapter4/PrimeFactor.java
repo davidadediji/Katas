2c8fcdda9a20c4679954948559cf2ecad666e6c3
davidadediji <davidadediji@gmail.com>
2022-08-01 21:49:39 +0100

    describe the changes here WIP - implement second test

--- src/main/java/chapter4/PrimeFactor.java
+++ src/main/java/chapter4/PrimeFactor.java
@@ -2,10 +2,12 @@ package chapter4;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PrimeFactor {
-    public List<Integer> test(int number) {
-        List<Integer> factors = new ArrayList<>();
-        return factors.add(number);
+    public Stack<Integer> getPrimeFactor(int number) {
+        Stack<Integer> primeFactors = new Stack<>();
+
+        return primeFactors;
     }
 }
--- src/test/java/chapter4/PrimeFactorTest.java
+++ src/test/java/chapter4/PrimeFactorTest.java
@@ -2,12 +2,25 @@ package chapter4;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrimeFactorTest {
     @Test
-    void shouldReturn2() {
+    void shouldReturnEmpty() {
         PrimeFactor primeFactor = new PrimeFactor();
-        assertEquals(factors, primeFactor.test(2));
+        Stack<Integer> expectedFactors = primeFactor.getPrimeFactor(1);
+        Stack<Integer> actualFactors = new Stack<>();
+        assertEquals(actualFactors, expectedFactors);
+    }
+
+    @Test
+    void shouldReturnTwo() {
+        PrimeFactor primeFactor = new PrimeFactor();
+        Stack<Integer> expectedFactors = primeFactor.getPrimeFactor(2);
+        Stack<Integer> actualFactors = new Stack<>();
+        actualFactors.push(2);
+        assertEquals(actualFactors, expectedFactors);
     }
 }
