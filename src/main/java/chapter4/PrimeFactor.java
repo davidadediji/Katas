bad85c8bef44d09f9ea4679cc8865834b81601bc
marienoir <okosunmaryeghonghon@gmail.com>
2022-08-01 22:41:25 +0100

    describe the changes here WIP - test implementation

--- src/main/java/chapter4/PrimeFactor.java
+++ src/main/java/chapter4/PrimeFactor.java
@@ -7,13 +7,19 @@ import java.util.Stack;
 public class PrimeFactor {
     public Stack<Integer> getPrimeFactor(int number) {
         Stack<Integer> primeFactors = new Stack<>();
-        int[] factor = {1, 2, 3, 5};
+        int[] factor = {2, 3, 5};
 
-        for (int value: factor){
-            if(number % value == 0){
-                primeFactors.push(value);
-            }
+        for (int i = 2; i < number; i++) {
+            if(number % i == 0 && number % 1 == 0){
+                    primeFactors.push(i);
+                }
+//            for (int value: factor){
+//                if(number % value == 0){
+//                    primeFactors.push(value);
+//                }
+//            }
         }
+
         return primeFactors;
     }
 }
