ffc58a14bd2ed87e352e4e1cb2c593f7d6dcc7fd
marienoir <okosunmaryeghonghon@gmail.com>
2022-08-03 13:51:49 +0100

    WIP -TicTacToe Tests Co-authored-by: davidadediji <davidadediji@gmail.com>

---
+++ src/main/java/chapter4/TicTacToe.java
@@ -0,0 +1,10 @@
+package chapter4;
+
+public class TicTacToe {
+    String playerValue;
+    public String getValue(String player) {
+        playerValue = player;
+        return playerValue;
+    }
+
+}
--- src/test/java/chapter4/TicTacToeTest.java
+++ src/test/java/chapter4/TicTacToeTest.java
@@ -1,4 +1,30 @@
 package chapter4;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TicTacToeTest {
+
+    @Test
+    void whenTheGameStartsItShouldBePlayerXTurn(){
+        TicTacToe ticTacToe = new TicTacToe();
+        String expectedValue = ticTacToe.getValue("X");
+        assertEquals("X", expectedValue);
+    }
+
+    @Test
+    void whenPlayerXMakesAMoveItShouldBePlayerOTurn(){
+        TicTacToe ticTacToe = new TicTacToe();
+        String expectedValue = ticTacToe.getValue("O");
+        assertEquals("O", expectedValue);
+    }
+
+    @Test
+    void whenPlayerOMakesAMoveItShouldBePlayerXTurn(){
+        TicTacToe ticTacToe = new TicTacToe();
+        String expectedValue = ticTacToe.getValue("O");
+        assertEquals("O", expectedValue);
+    }
+
+
 }
